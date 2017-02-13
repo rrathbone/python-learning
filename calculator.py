df15ffef6be9e9bf265147d1efb7bd92e3ec1087
rrathbone <rachellerathbone@gmail.com>
2017-02-12 21:21:06 -0800

    update to add max of 4 turns

--- calculator.py
+++ calculator.py
@@ -1,41 +1,41 @@
-"""Python is especially useful for doing math and can be used to automate many calculations. In this project, you'll create a calculator than can compute the area of a given shape, as selected by the user. The calculator will be able to determine the area of the following shapes:
+from random import randint
 
-Circle
-Triangle
-The program should do the following:
+board = []
 
-Prompt the user to select a shape
-Depending on the shape the user selects, calculate the area of that shape
-Print the area of that shape to the user"""
+for x in range(5):
+    board.append(["O"] * 5)
 
-from math import pi
-from time import sleep
-from datetime import datetime
+def print_board(board):
+    for row in board:
+        print " ".join(row)
 
-#.now() is accessing the now content from the imported datetime module
-now = datetime.now()
+print "Let's play Battleship!"
+print_board(board)
 
-print 'Starting the calculator...'
-print '%s-%s-%s' % (now.year, now.month, now.day)
+def random_row(board):
+    return randint(0, len(board) - 1)
 
-sleep(1)
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
 
-hint = 'Don\'t forget to include the correct units! \nExiting...'
+ship_row = random_row(board)
+ship_col = random_col(board)
+print ship_row
+print ship_col
 
-option = raw_input('Enter C for Circle or T for Triangle: ').upper()
+for turn in range(4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
 
-if option == 'C':
-    radius = float(raw_input("Enter radius: "))
-    area = pi * radius**2
-    print "The pie is baking..."
-    sleep(1)
-    print ("Area: %.2f. \n%s" % (area, hint))
-elif option == 'T':
-    base = float(raw_input("Enter base: "))
-    height = float(raw_input("Enter height: "))
-    area = (0.5)*base*height
-    print "Uni Bi Tri..."
-    sleep(1)
-    print ("Area: %.2f. \n%s" % (area, hint))
-else:
-	print 'You entered garbage so the program will now exit.'
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+        print "Turn", turn + 1
+        print_board(board)
